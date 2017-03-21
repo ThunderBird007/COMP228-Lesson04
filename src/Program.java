02d1fd1cc002f67ff4e13a780d26702922be1388
Alan Tse <starttodevelop@gmail.com>
2017-03-21 16:20:57 +1100

    Added simple menu

--- src/Program.java
+++ src/Program.java
@@ -5,8 +5,28 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Person person = new Person();
-		int myAge;
+		String prompt = "";
 
+		do {
+			System.out.println("Please make a selection:");
+			System.out.println("------------------------");
+			System.out.println("1: Enter your age");
+			System.out.println("2: Exit");
+			System.out.println("------------------------");
+			Scanner input = new Scanner(System.in);
+			prompt = input.nextLine();
+			
+			switch (prompt) {
+			case "1":
+				SetAge(person);
+				break;
+			}
+			
+		} while (!prompt.equals("2"));
+		System.out.println("Exit");
+	}
+
+	private static void SetAge(Person person) {
 		while (!person.getIsValid()) {
 			Scanner input = new Scanner(System.in);
 
